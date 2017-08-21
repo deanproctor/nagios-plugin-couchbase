1d0f10edf54ed8b170ce98d138038f221263605b
root <root@cbnode2.local>
2017-08-21 20:11:09 +0000

    checkpoint

--- check_couchbase.py
+++ check_couchbase.py
@@ -7,35 +7,37 @@ See: http://docs.couchbase.com/admin/admin/REST/rest-bucket-intro.html
 
 #### Dependencies
 
- * json
- * optparse
  * pyyaml
- * urllib2
 
 """
 
-import base64
-import urllib2 
-from optparse import OptionParser
+from argparse import ArgumentParser
+from base64 import b64encode
+from numbers import Number
 from subprocess import Popen, PIPE
+from urllib2 import Request, urlopen, HTTPError
 from yaml import load
 
 try:
-  import json
+  import json 
 except ImportError:
   import simplejson as json
 
-parser = OptionParser()
-parser.add_option('-c', dest='config_file', action='store')
-options, args = parser.parse_args()
 
-if(options.config_file is None):
-  print "Config file not specified.  Use -c CONFIG_FILE"
-  exit(1)
 
-yaml = file(options.config_file, 'r')
+# Setup command line arguments
+parser = ArgumentParser(usage = "usage: %prog [options] -c CONFIG_FILE")
+parser.add_argument('-c', '--config', dest='config_file', action='store', help='Path to the check_couchbase YAML file')
+args = parser.parse_args()
+
+if not args.config_file:
+  parser.error('Config file is required.  Use -c CONFIG_FILE')
+
+# Parse config file
+yaml = file(args.config_file, 'r')
 config = load(yaml)
 
+# Sends a passive check result to Nagios NSCA
 def send(service, status, message):
     line = "%s\t%s\t%d\t%s\n" % (config['couchbase_host'], service, status, message)
     print line
@@ -46,59 +48,92 @@ def send(service, status, message):
     pipe.stdin.close()
     pipe.wait()
 
-def getStats(bucket=None):
+# Executes a Couchbase REST API request and returns the output
+def couchbaseRequest(uri):
   host = config['couchbase_host']
   port = config['port']
   
-  if config['ssl'] is True:
+  if config['ssl']:
     protocol = 'https'
   else:
     protocol = 'http'
   
-  url = protocol + '://' + host + ':' + str(port) + '/pools/default/buckets/' 
+  url = protocol + '://' + host + ':' + str(port) + uri 
 
-  if bucket is not None:
-    url = url + bucket + '/stats'
+  auth_string = b64encode('%s:%s' % (config['user'], config['password']))
 
-  auth_string = base64.b64encode('%s:%s' % (config['user'], config['password']))
-
-  request = urllib2.Request(url);
+  request = Request(url);
   request.add_header("Authorization", "Basic %s" % auth_string)
 
   try:
-    f = urllib2.urlopen(request)
-    return json.load(f)
-  except urllib2.HTTPError, err:
-    print "Failed to get stats for bucket: " + bucket
-  
-def processMetrics(bucket, metrics):
-  data = getStats(bucket)
-  if not data:
-    return 
+    f = urlopen(request)
+    return load(f)
+  except HTTPError, err:
+    print 'Failed to complete request to Couchbase: ' + uri
 
-  samples = data['op']['samples']
+# Fetches, evalutes, and sends check results for bucket stats
+def processStats(bucket, metrics):
+  stats = couchbaseRequest('/pools/default/buckets/' + bucket + '/stats')
+  if not stats['op']['samples']:
+    print 'Failed to retrieve stats for bucket: ' + bucket
+
+  samples = stats['op']['samples']
 
   for m in metrics:
-    metric = m['metric']
-    avg_value = sum(samples[metric], 0.0) / len(samples[metric])
+    metric   = m['metric']
+    critical = m['crit']
+    warning  = m['warn']
+    value    = sum(samples[metric], 0.0) / len(samples[metric])
 
-    message = 'Bucket: ' + bucket + ', ' + metric + '= ' + str(avg_value)
+    # Build the Nagios service name
+    service = config['prefix']
 
-    if config['include_bucket_name'] is True:
-      service = 'CB ' + bucket + ' - ' + m['description']
+    if config['include_cluster_name']:
+      cluster_name = couchbaseRequest('/pools/default/')['clusterName']
+      if cluster_name:
+        service = service + ' ' + cluster_name
+
+    if config['include_bucket_name']:
+      service = service + ' ' + bucket
+
+    service = service + ' - ' + m['description']
+
+    # Evaluate the status and set the message
+#    if ((type(critical) is float or type(critical) is int or type(critical) is long or critical is None) and 
+#       (type(warning) is float or type(warning) is int or type(warning) is long or warning is None)):
+    if (isinstance(critical, Number) or critical is None) and (isinstance(warning, Number) or warning is None): 
+      if value >= critical:
+        status = 2
+        status_text = "CRITICAL"
+      elif value >= warning:
+        status = 1
+        status_text = "WARNING"
+      else:
+        status = 0
+        status_text = "OK"
     else:
-      service = 'CB ' + bucket + ' - ' + m['description']
+      if value in critical:
+        status = 2
+        status_text = "CRITICAL"
+      elif value in warning:
+        status = 1
+        status_text = "WARNING"
+      else:
+        status = 0
+        status_text = "OK"
 
-    status = 0
+    message = status_text + " - " + metric + ": " + str(value)
+
     send(service, status, message)
 
 def main():
   for bucket in config['buckets']:
+    # _all is a special case where we processStats for all buckets
     if bucket['name'] == '_all':
-      for b in getStats():
-        processMetrics(b['name'], bucket['metrics'])
+      for b in couchbaseRequest('/pools/default/buckets/'):
+        processStats(b['name'], bucket['metrics'])
     else:
-      processMetrics(bucket['name'], bucket['metrics'])
+      processStats(bucket['name'], bucket['metrics'])
         
 if __name__ == '__main__':
     main()
